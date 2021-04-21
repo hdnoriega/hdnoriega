54c7026a0678dce90a6234e9c346d4daabe6d8ce
hdnoriega <hnoriega@buenosaires.gob.ar>
2021-04-21 14:35:26 +0000

    segundo

--- demo/tools/GetCSVFromURL.py
+++ demo/tools/GetCSVFromURL.py
@@ -2,7 +2,7 @@ import requests
 
 def getCSV(path, name):
     r = requests.get(path, allow_redirects=True)
-    destination = '/demo/tools/downloaded/' + name
+    destination = '/home/comunistanazi/hdnoriega/demo/tools/downloaded/' + name
     open(destination, 'wb').write(r.content)
     return destination
 
@@ -10,4 +10,4 @@ def getCSV(path, name):
 # Test, if it works, you will find the file in the "downloaded" directory.
 # For this example, i am using a small .csv file i found on the internet.
 
-#getCSV('https://www.stats.govt.nz/assets/Uploads/Business-price-indexes/Business-price-indexes-September-2020-quarter/Download-data/business-price-indexes-september-2020-quarter-corrections-to-previously-published-statistics.csv', 'downloadedCSV.csv')
+getCSV('https://www.stats.govt.nz/assets/Uploads/Business-price-indexes/Business-price-indexes-September-2020-quarter/Download-data/business-price-indexes-september-2020-quarter-corrections-to-previously-published-statistics.csv', 'downloadedCSV.csv')
